d890fe3a6eaf34d3a9d6140b145015dd5c009f63
dongfang <dongfang@qq.com>
2022-05-10 08:25:01 +0800

    first commit

---
+++ src/test/java/GitTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ *
+ * Git集成IDEA的测试
+ * @Author shkstart
+ * @create 2022-05-10-8:23
+ */
+public class GitTest {
+
+    @Test
+    public void demo1(){
+        System.out.println("hello git!");
+    }
+}
